e3fb94649a75a9c60a6f5e725aa92a8c57cb901d
Patrick Beedgen <patrick.beedgen@googlemail.com>
2014-10-04 02:12:37 +0200

    initial commit

---
+++ SharpExtensions/SharpStringExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpExtensions
+{
+    public static class SharpStringExtensions
+    {
+        /// <summary>
+        /// Replaces the first occurrence of a certain string in a source string
+        /// </summary>
+        /// <param name="text">The original string</param>
+        /// <param name="target">target for replacement</param>
+        /// <param name="replaceWith"></param>
+        /// <returns></returns>
+        public static string ReplaceFirst(this string text, string target, string replaceWith)
+        {
+            int pos = text.IndexOf(target);
+            if (pos < 0)
+            {
+                return text;
+            }
+            return text.Substring(0, pos) + replaceWith + text.Substring(pos + target.Length);
+        }
+
+        /// <summary>
+        /// Replaces the last occurrence of a certain string in a source string
+        /// </summary>
+        /// <param name="text">The original string</param>
+        /// <param name="target">target for replacement</param>
+        /// <param name="replaceWith"></param>
+        /// <returns></returns>
+        public static string ReplaceLast(this string text, string target, string replaceWith)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tests/StringExtensionTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharpExtensions;
+
+namespace Tests
+{
+    [TestClass]
+    public class StringExtensionTests
+    {
+        [TestMethod]
+        public void ReplaceFirstTestSimple()
+        {
+            string example = "Hello World!";
+            string target = "Ho World!";
+            Assert.AreEqual(example.ReplaceFirst("ell", ""),target);
+        }
+    }
+}
